031c7efe2ed54b19f7ab23f131b634afe3e26a00
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-14 21:07:40 +0200

    fix: Corrected typo in action types feat: Implemented fetchUsers method
    - Added `fetchUsers` method in `userActions.ts` to handle API requests using axios.
      - Fetches user data from the API and maps it to the `User` interface.
      - Dispatches appropriate actions for request, success, and failure states.

--- src/redux/user/userActions.ts
+++ src/redux/user/userActions.ts
@@ -1,5 +1,6 @@
 import { FETCH_USERS_FAILURE, FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS } from "./userTypes"
 import { User } from "../../interfaces/User"
+import axios from "axios"
 
 export const fetchUsersRequest = () => {
     return {
@@ -18,5 +19,27 @@ const fetchUsersFailure = (error : string) => {
     return {
         type: FETCH_USERS_FAILURE,
         payload: error
+    }
+}
+
+
+export const fetchUsers = () => {
+    return (dispatch : any) => {
+        dispatch(fetchUsersRequest)
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                const users : User[] = response.data.map((user :any) => ({
+                    id: user.id,
+                    name: user.name,
+                    username: user.username,
+                    email: user.email,
+                    phone: user.phone
+                }))
+                dispatch(fetchUsersSuccess(users))
+            })
+            .catch( error => {
+                const errorMsg : string = error.message
+                dispatch(fetchUsersFailure(errorMsg))
+            })
     }
 }
