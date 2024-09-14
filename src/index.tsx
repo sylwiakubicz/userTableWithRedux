4d558211ed3f4ae6b8dd986a00c531e6a976f03a
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-14 22:27:07 +0200

    feat: Add Redux store and Provider to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 export * from './redux/user/userActions'
+import store from './store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <Provider store={store}>
     <App />
+  </Provider>
 );
 
