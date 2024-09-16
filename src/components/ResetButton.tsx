230b6a8eed6e6077d098559e1ac91122f9f00dae
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-16 21:19:13 +0200

    chore: remove unused imports and organize variables.

--- src/components/ResetButton.tsx
+++ src/components/ResetButton.tsx
@@ -1,4 +1,3 @@
-import { Filters } from '../interfaces/filters'
 import { AppDispatch, RootState } from '../redux/store';
 import { useDispatch, useSelector } from 'react-redux';
 import { resetUsersFilters } from '../redux/usersFilters/userFiltersSlice';
@@ -7,10 +6,9 @@ import { resetUsersFilters } from '../redux/usersFilters/userFiltersSlice';
 function ResetButton() {
 
     const usersFilters = useSelector((state : RootState) => state.userFilters)
-    const dispatch = useDispatch<AppDispatch>();
-
-
     const hasActiveFilters : boolean = Object.values(usersFilters).some((value) => value !== '');
+    const dispatch = useDispatch<AppDispatch>();
+    
     return (
         <button 
             className={"text-end pr-6 pb-2 font-light capitalize cursor-pointer opacity-60 hover:opacity-100 " + 
@@ -19,7 +17,6 @@ function ResetButton() {
         >
             Reset filters
         </button>
-
     )
 }
 
--- src/components/SideNavHeader.tsx
+++ src/components/SideNavHeader.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
 function SideNavHeader({ title }: { title: string }) {
-  return (
-    <h1 className="font-semibold text-3xl pl-3 dark:text-white pb-5">
-      {title}
-    </h1>
-  )
+        return (
+        <h1 className="font-semibold text-3xl pl-3 dark:text-white pb-5">
+            {title}
+        </h1>
+    )
 }
 
 export default SideNavHeader
