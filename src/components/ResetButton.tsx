c5ad3e50209a9a55978711e7cd21b82e3aa26220
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-16 20:04:17 +0200

    feat: implement ResetButton to clear user filters in UserTable

--- src/components/ResetButton.tsx
+++ src/components/ResetButton.tsx
@@ -1,18 +1,21 @@
 import { Filters } from '../interfaces/filters'
+import { AppDispatch, RootState } from '../redux/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { resetUsersFilters } from '../redux/usersFilters/userFiltersSlice';
 
-interface ResetButtonProps {
-    filters: Filters;
-    handleReset: () => void;
-}
 
-function ResetButton({filters, handleReset} : ResetButtonProps) {
+function ResetButton() {
 
-    const hasActiveFilters : boolean = Object.values(filters).some((value) => value !== '');
+    const usersFilters = useSelector((state : RootState) => state.userFilters)
+    const dispatch = useDispatch<AppDispatch>();
+
+
+    const hasActiveFilters : boolean = Object.values(usersFilters).some((value) => value !== '');
     return (
         <button 
             className={"text-end pr-6 pb-2 font-light capitalize cursor-pointer opacity-60 hover:opacity-100 " + 
                 (hasActiveFilters ? 'show' : 'hidden')} 
-            onClick={() => handleReset()}
+            onClick={() => dispatch(resetUsersFilters())}
         >
             Reset filters
         </button>
