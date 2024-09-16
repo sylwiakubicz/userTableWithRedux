de0b91df0be29433d9b79bc9e9184ceb9bd46b27
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-09-16 21:06:58 +0200

    refactor: move SideNav to App component and update AccountsListPage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import AccountsListPage from './components/AccountsListPage';
+import SideNav from './components/SideNav';
 
 
 function App() {
   return (
-      <AccountsListPage />
+    <div className='bg-primary dark:bg-dark-primary min-h-screen'>
+      <SideNav />
+      <main className="ml-[15rem] p-5">
+        <AccountsListPage />
+      </main>
+    </div>
   );
 }
 
--- src/components/AccountsListPage.tsx
+++ src/components/AccountsListPage.tsx
@@ -1,16 +1,11 @@
-import React from 'react'
 import UserTable from './UserTable';
-import SideNav from './SideNav';
 
 function AccountsListPage() {
     return (
-    <div className="bg-primary dark:bg-dark-primary min-h-screen">
-        <SideNav />
-        <div className="ml-[15rem] p-5">
+        <div >
             <h2 className="text-3xl text-secondary dark:text-white pb-5">User Accounts</h2>
             <UserTable />
         </div>
-    </div>
     )
 }
 
